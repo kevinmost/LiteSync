0f5b76c08e1883f0b44882f8b46580f684c8d24a
Kevin Most <kevin.most.93@gmail.com>
2014-02-08 20:35:13 -0500

    Python is awful

--- flask/hello.py
+++ flask/hello.py
@@ -5,13 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	
 	GPIO.setup(18, GPIO.OUT)
 	GPIO.output(18, False)
 	time.sleep(2)
 	GPIO.output(18, True)
 	time.sleep(2)
-    return "Welcome to LiteSync!"
+	return "Welcome to LiteSync!"
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
