1be7831d4df31aed1cf852a2f5d9da02a6b7759e
Kevin Most <kevin.most.93@gmail.com>
2014-02-08 20:44:01 -0500

    copypasta

--- flask/hello.py
+++ flask/hello.py
@@ -1,16 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 import RPi.GPIO as GPIO
 app = Flask(__name__)
 
+GPIO.setmode(GPIO.BCM)
 
 @app.route("/")
 def hello():
-	GPIO.setup(18, GPIO.OUT)
-	GPIO.output(18, False)
-	time.sleep(2)
-	GPIO.output(18, True)
-	time.sleep(2)
-	return "Welcome to LiteSync!"
+   now = datetime.datetime.now()
+   timeString = now.strftime("%Y-%m-%d %H:%M")
+   templateData = {
+      'title' : 'HELLO!',
+      'time': timeString
+      }
+   return render_template('main.html', **templateData)
+
+@app.route("/readPin/<pin>")
+def readPin(pin):
+   try:
+      GPIO.setup(int(pin), GPIO.IN)
+      if GPIO.input(int(pin)) == True:
+         response = "Pin number " + pin + " is high!"
+      else:
+         response = "Pin number " + pin + " is low!"
+   except:
+      response = "There was an error reading pin " + pin + "."
+
+   templateData = {
+      'title' : 'Status of Pin' + pin,
+      'response' : response
+      }
+
+   return render_template('pin.html', **templateData)
+
 
 if __name__ == "__main__":
-	app.run()
+   app.run(host='0.0.0.0', port=80, debug=True)
