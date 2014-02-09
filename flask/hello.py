dbdf8bf03a530fb3b157589401c8f0211b61435f
Kevin Most <kevin.most.93@gmail.com>
2014-02-08 21:02:59 -0500

    Moved pin.html template

--- flask/hello.py
+++ flask/hello.py
@@ -21,19 +21,22 @@ def readPin(pin):
    try:
       GPIO.setup(int(pin), GPIO.IN)
       if GPIO.input(int(pin)) == True:
-         response = "Pin number " + pin + " is high!"
+         return "Pin " + pin + " is high"
+         # response = "Pin number " + pin + " is high!"
       else:
-         response = "Pin number " + pin + " is low!"
+         return "Pin " + pin + " is low"
+         # response = "Pin number " + pin + " is low!"
    except:
-      response = "There was an error reading pin " + pin + "."
+      return "There was an error reading pin" + pin
+      #response = "There was an error reading pin " + pin + "."
 
-   templateData = {
-      'title' : 'Status of Pin' + pin,
-      'response' : response
-      }
+   #templateData = {
+   #   'title' : 'Status of Pin' + pin,
+   #   'response' : response
+   #   }
 
-   return render_template('pin.html', **templateData)
-
+   #return render_template('pin.html', **templateData)
+   return 0
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=5000, debug=True)
